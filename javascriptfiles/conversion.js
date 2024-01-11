9d14ed58f806873f7155657edeaf3dc39dedc511
roshnigupta9718 <131439426+roshnigupta9718@users.noreply.github.com>
2024-01-11 15:23:58 +0000

    conersion in js

---
+++ javascriptfiles/conversion.js
@@ -0,0 +1,15 @@
+let somenumber="33"
+let loggedin=Number(somenumber)
+console.log(loggedin)
+console.log(typeof loggedin)
+// conversion in boolean
+// let someboolean=1
+// let bool=Boolean(someboolean)
+// console.log(bool)
+let number=9
+let numberinstring=String(number)
+
+console.log(numberinstring)
+console.log(typeof numberinstring)
+
+
