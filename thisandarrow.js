3333a2512612812e73ccbc6682ada33da2f5dd7e
roshnigupta9718 <131439426+roshnigupta9718@users.noreply.github.com>
2024-01-16 05:53:53 +0000

    this

---
+++ thisandarrow.js
@@ -0,0 +1,19 @@
+// const user={
+//     username:"roshni",
+//     price:9999,
+//     welcomemessage:function(){
+//       console.log(  `${this.username}welcome to website`)
+//     }
+// }
+// user.welcomemessage()
+// user.username="ragini"
+// user.welcomemessage()
+// again practice .....................
+const user={
+    username:"sarvesh",
+    wel:function(){
+        console.log(`hello ,${this.username}`)
+    }
+}
+user.username="ragini"
+user.wel()
